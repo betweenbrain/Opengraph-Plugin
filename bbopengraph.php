6af75c76157b6aa3b77679b882e841edcc4ad24e
betweenbrain <matt@betweenbrain.com>
2012-06-29 13:23:25 -0400

    Working on implementing proper check of scope

--- bbopengraph.php
+++ bbopengraph.php
@@ -28,11 +28,10 @@ class plgContentBbOpenGraph extends JPlugin
 		$doc    = JFactory::getDocument();
 		$option = JRequest::getVar('option', '');
 		$view   = JRequest::getVar('view', '');
+		$scope  = $option . '.' . $view;
 
-		echo '<pre>View: ' . $view . '<br/>Option: ' . $option . '<br/>Context: ' . $context . '<br/></pre>';
-
-		// Ensure that we are not in the back-end, or haven't run before, and are in the right context
-		if (!$app->isAdmin() && ($count == 0) && ($context == 'com_content.article')) {
+		// Ensure that we are not in the back-end, or haven't run before in category views, and are in the right scope. Phew!
+		if (!$app->isAdmin() && ($count == 0) && (in_array($scope, array('com_content.article', 'com_content.category', 'com_content.featured')))) {
 
 			// The canonical URL of the page.
 			$doc->setMetadata('og:url', JURI::current());
