a72c2a30680246fedcc1487caf914d1a04c096a2
betweenbrain <matt@betweenbrain.com>
2012-06-28 22:30:47 -0400

    Code formatting and annotations Debug code commented out

--- bbopengraph.php
+++ bbopengraph.php
@@ -1,8 +1,16 @@
 <?php defined('_JEXEC') or die;
 
+/**
+ * Simple plugin for implementing the Open Graph protocol - http://ogp.me/
+ *
+ * @package		Joomla.Plugin
+ * @copyright	Copyright (C) 2012 Matt Thomas http://betweenbrain.com. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ *
+ */
+
 jimport('joomla.plugin.plugin');
 
-// http://ogp.me/
 class plgContentBbOpenGraph extends JPlugin
 {
 
@@ -19,7 +27,7 @@ class plgContentBbOpenGraph extends JPlugin
 
 		$app  = JFactory::getApplication();
 		$doc  = JFactory::getDocument();
-		$view = JRequest::getCmd('view');
+		// $view = JRequest::getCmd('view');
 
 		if ($app->isAdmin()) {
 			return true;
@@ -30,24 +38,43 @@ class plgContentBbOpenGraph extends JPlugin
 		}
 
 		// TODO: Figure out accessing only first row of blog views
+
 		// if ($view == 'featured' && $this->count == 0) { }
-		//var_dump($row);
 
-		$quotes = array('"', "'");
-		$title  = str_replace($quotes, '', $row->title);
+		// var_dump($row);
 
+		// var_dump(get_object_vars($row));
+
+		/*
+		$rows = get_object_vars($row);
+		foreach ($rows as $key => $value) {
+			return 'Key: ' . $key . ' Value: ' . $value;
+		}
+		*/
+
+		// The canonical URL of the page.
 		$doc->setMetadata('og:url', JURI::current());
+
+		// The type of object.
 		$doc->setMetadata('og:type', 'article');
 
-		if (isset($row->title))
+		// The title of the object.
+		$title = str_replace(array('"', "'"), '', $row->title);
+
+		if (isset($row->title)) {
 			$doc->setMetadata('og:title', htmlspecialchars($title));
+		}
 
-		if (isset($row->introtext))
+		// A description of the object.
+		if (isset($row->introtext)) {
 			$doc->setMetadata('og:description', substr(strip_tags($row->introtext), 0, 255));
+		}
 
-		preg_match('/<img.+src=[\'"]([^\'"]+)[\'"].*>/i', $row->introtext . $row->introtext, $image);
+		// An image URL which represents the object
+		preg_match('/<img.+src=[\"]([^\"]+)[\"].*>/i', $row->introtext, $image);
 
-		if (isset($image[1]) && $image[1] != '')
+		if (isset($image[1]) && $image[1] != '') {
 			$doc->setMetadata('og:image', JURI::base() . $image[1]);
+		}
 	}
 }
